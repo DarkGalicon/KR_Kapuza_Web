205f9781003c571a06a2ba752dc823497a15bfc3
DarkGalicon <vlas_kapuza21@mail.ru>
2024-06-18 00:43:34 +0300

    modified:   controllers/arrayController.js 	modified:   models/array.js

--- models/array.js
+++ models/array.js
@@ -1,18 +1,22 @@
-const arrays = [];
+var arrays = [];
 
 module.exports = class Array{
 
+    
     constructor(num1, num2, num3){
-        this.num1 = num1
-        this.num2 = num2
-        this.num3 = num3
-    }
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+    };
 
     save(){
-        this.push(arrays)
-    }
+        arrays.push(this)
+        console.log("Сохраненно успешно")
+    };
 
     static getAll(){
+        console.log(arrays);
         return arrays;
+        
     };
 }
